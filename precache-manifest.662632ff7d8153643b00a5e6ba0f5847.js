8f2cbab190f9886c51f5adea6455673cb29f31d6
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-18 11:03:22 +0000

    Updates

---
+++ precache-manifest.662632ff7d8153643b00a5e6ba0f5847.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbd99d251efa4d2dd91d228437810a8",
+    "url": "/seasonal-cuisine/static/media/cookies2.4cbd99d2.jpg"
+  },
+  {
+    "revision": "d09b1037bff2704d0c1c",
+    "url": "/seasonal-cuisine/static/css/main.bcec5bf8.chunk.css"
+  },
+  {
+    "revision": "8fd4831e1c2ea37f9048",
+    "url": "/seasonal-cuisine/static/js/1.8fd4831e.chunk.js"
+  },
+  {
+    "revision": "0784e9ce00cc1b9a6344",
+    "url": "/seasonal-cuisine/static/js/runtime~main.0784e9ce.js"
+  },
+  {
+    "revision": "9c3dc666a3d4971cbd342024c587ea39",
+    "url": "/seasonal-cuisine/static/media/chicken.9c3dc666.jpg"
+  },
+  {
+    "revision": "1fd31968719c79f60be7fc6c2b318c38",
+    "url": "/seasonal-cuisine/static/media/cookies.1fd31968.jpg"
+  },
+  {
+    "revision": "d09b1037bff2704d0c1c",
+    "url": "/seasonal-cuisine/static/js/main.d09b1037.chunk.js"
+  },
+  {
+    "revision": "bb5d028b0fed3945c5be90ee280852d5",
+    "url": "/seasonal-cuisine/static/media/meat.bb5d028b.jpg"
+  },
+  {
+    "revision": "db87f7696ec531d5ce39ec9aa416d070",
+    "url": "/seasonal-cuisine/static/media/meat2.db87f769.jpg"
+  },
+  {
+    "revision": "69e02093b55540027b72b93d8f27d29e",
+    "url": "/seasonal-cuisine/static/media/logoSC.69e02093.png"
+  },
+  {
+    "revision": "b8697c3321255f6dabe191c2c320a6a8",
+    "url": "/seasonal-cuisine/static/media/hero-img.b8697c33.jpg"
+  },
+  {
+    "revision": "2471c7dbb85b6f4657b81ef6e67ce29d",
+    "url": "/seasonal-cuisine/index.html"
+  }
+];
